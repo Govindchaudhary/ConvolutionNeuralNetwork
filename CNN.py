2d9164937a73e2732cbe1215984a2453ed04dd29
GovindChaudhary <govindchaudhary2404@gmail.com>
2018-04-10 23:49:23 +0530

    comments added

--- CNN.py
+++ CNN.py
@@ -5,22 +5,29 @@ from keras.layers import Flatten
 from keras.layers import Dense
 
 
-#initializing the classifier
+#initializing the CNN (2 methods defining the sequence of layers or defining a graph)
+# we will go with the defining the sequence of layers
 classifier = Sequential()
 
 #step-1 convolution layer
 #here 32 is the no. of feature detector and 3,3 is the size
 #input_shape is the what to expect in input (here 64,64 sized color image 3 indicates colored)
+#relu is the activation func to be used ie. f(x)=max(x,0)
 
 classifier.add(Convolution2D(32,3,3, input_shape=(64,64,3),activation='relu'))
 
 #step-2 Pooling
-#basically reducing the no. of node that will get i  flatteing step ie. to reduce the size of feature maps
+#basically reducing the no. of node that will get into  flatteing step ie. to reduce the size of feature maps
 #avoiding the expensive computation
+#avoids the chances of overfitting and also maintained the special features of the image
+#main purpose is the spatial invariance ie. our model can recognize the same image at diff. angle  and position
+#here pool_size is basically the box of size 2 x 2 so we are moving this box to the whole convoluted matrix with the stride of 2
+#and we take the max of the four number and put it into our pooled matrix
+
 
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
-#ading a second convolution layer
+#adding a second convolution layer
 #no need of input_shape bcos it gets input from pooled step1
 #this is a simple way to inc the accuracy without inc the cost of computation
 
